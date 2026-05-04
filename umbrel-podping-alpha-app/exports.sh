85b8d4a16a7a89937887ffea5da1adce5227718c
Eric P <ericpp@gmail.com>
2026-05-03 23:57:14 -0500

    0.6.0.9

--- umbrel-podping-alpha-app/exports.sh
+++ umbrel-podping-alpha-app/exports.sh
@@ -1,8 +1,6 @@
 
 if [ -f "${EXPORTS_APP_DIR}/data/node_friendly_name.txt" ]; then
-    read -r NODE_FRIENDLY_NAME < "${EXPORTS_APP_DIR}/data/node_friendly_name.txt"
-    export NODE_FRIENDLY_NAME
+    read -r APP_NODE_FRIENDLY_NAME < "${EXPORTS_APP_DIR}/data/node_friendly_name.txt"
+    export APP_NODE_FRIENDLY_NAME
     echo "Node friendly name: $NODE_FRIENDLY_NAME"
 fi
-
-export SSE_ENABLED=1
