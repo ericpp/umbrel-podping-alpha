d8807e2adf0962394d90533ff24d4b23fdbf1df5
Eric P <ericpp@gmail.com>
2026-05-03 23:50:56 -0500

    0.6.0.8

--- umbrel-podping-alpha-app/exports.sh
+++ umbrel-podping-alpha-app/exports.sh
@@ -1,9 +1,5 @@
-echo "umbrel data dir: $EXPORTS_APP_DIR"
-
-ls -la $EXPORTS_APP_DIR
 
 if [ -f "${EXPORTS_APP_DIR}/data/node_friendly_name.txt" ]; then
-    # Use 'read' to safely read the first line
     read -r NODE_FRIENDLY_NAME < "${EXPORTS_APP_DIR}/data/node_friendly_name.txt"
     export NODE_FRIENDLY_NAME
     echo "Node friendly name: $NODE_FRIENDLY_NAME"
